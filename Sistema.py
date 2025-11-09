f91fd0bf7c4afc86c724e093ab201dbff2084394
Vinicius Santos <vinobeacoup56@gmail.com>
2025-11-08 23:47:14 -0300

    Refactor age and hours validation logic

--- Sistema.py
+++ Sistema.py
@@ -1,16 +1,24 @@
-idade = int(input("Qual sua idade? "))
-if idade >= 18:
-    print("Voce é maior de idade")
-else:
-    print("voce é menor de idade")
+print("----Bem vindo a Lan House!----")
+print("|Para Jogar, voce precisa ter 18 anos!")
+def Sis():
+    while True:
+        global idade
+        idade = int(input("|Qual sua idade?:  "))
+        if idade >= 18:
+            print("Voce é maior de idade")
+            Horas()
+            break
+        else:
+            print("voce é menor de idade")
+def Horas(): 
+    horario = int(input("Que horas são? (0-23) "))
+    if horario < 23:
+        print("ainda nao deu a hora do estabelecimento fechar")
+    else:
+        print("Ja passou da hora")
 
-horario = int(input("Que horas são? (0-23) "))
-if horario < 22:
-    print("ainda nao deu a hora do estabelecimento fechar")
-else:
-    print("Ja passou da hora")
+    creditos = float(input("Quantos créditos você tem? R$ "))
 
-creditos = float(input("Quantos créditos você tem? R$ "))
-
-if idade >= 16 and horario < 22 and creditos >= 8.0:
-    print("Voce esta liberado para jogar")
+    if idade >= 16 and horario < 22 and creditos >= 8.0:
+        print("Voce esta liberado para jogar")
+Sis()
